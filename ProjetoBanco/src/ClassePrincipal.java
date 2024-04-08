a67f776231b44ee13b5a32b97256041d1fcdc3d4
etec <etec@prof04>
2024-04-08 20:16:08 -0300

    Terceiro commit

--- ProjetoBanco/src/ClassePrincipal.java
+++ ProjetoBanco/src/ClassePrincipal.java
@@ -19,6 +19,16 @@ public class ClassePrincipal {
 		System.out.println(objeto_conta_dois.atributoTipoConta);
 		System.out.println(objeto_conta_dois.atributoNomeCliente);
 		System.out.println(objeto_conta_dois.atributoDocumentoCliente);
+		
+		// Adicionado dados das contas.
+		objeto_conta_um.atributoNumeroConta = 123;
+		objeto_conta_um.atributoSaldoConta = 2_600.00F;
+		objeto_conta_um.atributoTipoConta = "Conta Poupança";
+		
+		objeto_conta_dois.atributoNumeroConta = 789;
+		objeto_conta_dois.atributoSaldoConta = 4_000.00F;
+		objeto_conta_dois.atributoTipoConta = "Conta Coorente";
+		
 	}
 
 }
